0e3ae5d0b9e3d3d55f7fa960dc77cd907c3e24b0
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-05-05 06:59:33 +0200

    Supprimer le dossier script

--- script/a-propos/theme-toggle.js
+++
@@ -1,5 +0,0 @@
-// JavaScript pour le changement de thème
-document.getElementById('theme-toggle').addEventListener('change', function () {
-    document.body.setAttribute('data-theme', this.checked ? 'dark' : 'light');
-});
-
